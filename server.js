c4679460dc3581a116a7dca06aa6260cb0805d42
Justin Marchi <marchij86@gmail.com>
2020-04-25 18:48:26 -0400

    Delete function working

--- server.js
+++ server.js
@@ -13,56 +13,84 @@ const writeFile = util.promisify(fs.writeFile);
 // Set up Express
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 
 // HTML Routes
-app.get("/", function(req, res) {
+// Home Page Route
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
-app.get("/notes", function(req, res) {
+// Note Taker Route
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 
 // API GET Route
-const noteData = require("./db/db.json");
-
-app.get("/api/notes" , function(req, res) {
-    
-    
-   return res.json(noteData);
+app.get("/api/notes", async function (req, res) {
+    const noteData = await getNotes();
+    return res.status(200).json(noteData);
 });
 
 
 // API POST Route
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", async function (req, res) {
     let newNote = req.body;
-    for (i=0; i<noteData.length+1; i++) {
-        newNote.id = i+1;
+    const noteData = await getNotes();
+
+    noteData.push({
+        ...newNote,
+        id: noteData.length + 1
+    });
+    await writeNotes(noteData);
+    return res.sendStatus(201);
+});
+
+// API DELETE Route
+app.delete("/api/notes/:id", async function (req, res) {
+    let noteID = Number(req.params.id);
+    const noteData = await getNotes();
+
+    const newNotes = noteData.reduce((acc, curr) => {
+        if (curr.id !== noteID) acc.push(curr);
+        return acc;
+    }, []);
+
+    await writeNotes(newNotes);
+    return res.sendStatus(200);
+});
+
+// Read function
+const getNotes = async () => {
+    try {
+        const data = await readFile(path.join(__dirname, './db/db.json'), 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        return err;
     }
-    noteData.push(newNote);
-    res.json(newNote);
-});
+};
 
-app.delete("/api/notes/:id", function(req, res) {
-    let noteID = req.params.id;
-    
-    for (i=0; i<noteData.length; i++) {
-        if (noteID === noteData[i].id) {
-            noteData.splice(i, 1);
-        };
-    };
-
-    res.json(noteData);
-});
-
+// Write function
+const writeNotes = async (data) => {
+    try {
+        const update = await writeFile(
+            path.join(__dirname, './db/db.json'),
+            JSON.stringify(data)
+        );
+        return update;
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+};
 
 // Initiate Listener to start server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
